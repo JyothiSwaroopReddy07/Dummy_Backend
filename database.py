ab684a42100f2c2acb7bb4e034d21cdaabcda399
JyothiSwaroopReddy07 <bjyothiswaroop2002@gmail.com>
2025-03-26 10:54:17 -0700

    commit

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://gasstation_vqo1_user:IgcV8M86MP058fjt1r4hIACFfe4JPUKD@dpg-cvhofp2qgecs73d2tie0-a.oregon-postgres.render.com/gasstation_vqo1")
+# Load environment variables from .env file
+load_dotenv()
+
+# Fetch the database URL from the environment
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
